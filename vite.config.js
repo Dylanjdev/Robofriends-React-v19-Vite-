8fbe570955135b28abe7cfc13131f2ae6f88f85a
Dylan Smith <dylanjsmith.dev@gmail.com>
2025-08-25 21:55:01 -0400

    updated package.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Dylanjdev/Robofriends-React-v19-Vite-.git"
+  base: "/Robofriends-React-v19-Vite-"
 })
